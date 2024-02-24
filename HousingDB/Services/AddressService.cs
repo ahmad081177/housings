1bd188b370d98f64c3d41e8d19d1a35e692ec0e7
ahmad081177 <ahmad081177@gmail.com>
2024-02-24 11:24:52 +0200

    add more features

---
+++ HousingDB/Services/AddressService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using System.Text.Json;
+
+namespace HousingDB.Services
+{
+    public class AddressService
+    {
+        public async Task<Tuple<double, double>> AddressToLatLong(string address)
+        {
+            try
+            {
+                //call Http to: https://nominatim.openstreetmap.org/search?addressdetails=1&q={address}&format=jsonv2&limit=1
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("User-Agent", "C# console program");
+                var resp = await client.GetAsync($"https://nominatim.openstreetmap.org/search?addressdetails=1&q={address}&format=jsonv2&limit=1");
+                //read resp as json
+                string sresp = await resp.Content.ReadAsStringAsync();
+                //parse the json to get lat and long
+                var jresp = JsonSerializer.Deserialize<List<Dictionary<string, object>>>(sresp);
+                double lon = double.Parse(jresp[0]["lon"].ToString());
+                double lat = double.Parse(jresp[0]["lat"].ToString());
+                return new Tuple<double, double>(lat, lon);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return new Tuple<double, double>(0, 0);
+            }
+        }
+    }
+}
---
+++ HousingDB/Services/EmailService.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Net;
+using System.Net.Mail;
+
+namespace HousingDB.Services
+{
+    //See https://docs.google.com/document/d/1-naW1AW-xv_bhJFC1KHo_QbQrtiu_YCzgkOY2fn_GEM/edit#heading=h.51lh3iio1qpt
+    public class EmailService
+    {
+        private IConfiguration configuration;
+        public EmailService(IConfiguration configuration)
+        {
+            configuration = configuration;
+        }
+        public async Task SendEmail(string to, string subject, string body, bool isBodyHTML = true)
+        {
+            IConfigurationSection config = configuration.GetSection("Email");
+            if(config!=null)
+            {
+                //read from the config from.mail, from.display, password
+                string from = config["from:mail"];
+                string display = config["from:display"];
+                string emailPassword = config["password"];
+
+                if (string.IsNullOrEmpty(emailPassword) || string.IsNullOrEmpty(from))
+                    throw new Exception("Missing Email section in the appsettings");
+
+                var fromEmail = new MailAddress(from, display);
+                var toEmail = new MailAddress(to);
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    Timeout = 5000,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromEmail.Address, emailPassword)
+                };
+                using var message = new MailMessage(fromEmail, toEmail)
+                {
+                    Subject = subject,
+                    Body = body,
+                    IsBodyHtml = isBodyHTML,
+                };
+                try
+                {
+                    await smtp.SendMailAsync(message);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
